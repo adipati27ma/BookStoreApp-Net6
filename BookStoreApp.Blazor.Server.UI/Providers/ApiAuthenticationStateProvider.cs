4694bd11d9128f1595f4a8b2fb74792dd9caaf80
adipati27ma <adipati.cs.samsung@gmail.com>
2022-06-21 10:31:04 +0700

    [Add] custom api auth provider

--- BookStoreApp.Blazor.Server.UI/Providers/ApiAuthenticationStateProvider.cs
+++ BookStoreApp.Blazor.Server.UI/Providers/ApiAuthenticationStateProvider.cs
@@ -18,21 +18,55 @@ namespace BookStoreApp.Blazor.Server.UI.Providers
 
 		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
 		{
+			// standard empty user
+			var user = new ClaimsPrincipal(new ClaimsIdentity());
 			// get token in local storage
 			var savedToken = await localStorage.GetItemAsync<string>("accessToken");
 
 			// if the token is null, return new claims with the empty user & out the func.
 			if (savedToken == null)
 			{
-				return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+				return new AuthenticationState(user);
 			}
 
+			// Checking expiration token
 			var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
-			var claims = tokenContent.Claims;
+			if (tokenContent.ValidTo < DateTime.Now)
+			{
+				return new AuthenticationState(user);
+			}
 
-			var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+			var claims = tokenContent.Claims;
+			user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
 
 			return new AuthenticationState(user);
 		}
+
+		public async Task LoggedIn()
+		{
+			var savedToken = await localStorage.GetItemAsync<string>("accessToken");
+			var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+			var claims = tokenContent.Claims;
+			var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+			var authState = Task.FromResult(new AuthenticationState(user));
+			NotifyAuthenticationStateChanged(authState);
+		}
+
+		public async Task LoggedOut()
+		{
+			//await localStorage.RemoveItemAsync("accessToken");
+			var nobody = new ClaimsPrincipal(new ClaimsIdentity());
+			var authState = Task.FromResult(new AuthenticationState(nobody));
+			NotifyAuthenticationStateChanged(authState);
+		}
+
+		//private async Task<List<Claim>> GetClaims()
+		//{
+		//	var savedToken = await localStorage.GetItemAsync<string>("accessToken");
+		//	var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+		//	var claims = tokenContent.Claims.ToList();
+		//	claims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+		//	return claims;
+		//}
 	}
 }
--- BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -1,5 +1,9 @@
 ﻿using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using Microsoft.AspNetCore.Components.Authorization;
+
+
 
 namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
 {
@@ -7,11 +11,13 @@ namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
 	{
 		private readonly IClient httpClient;
 		private readonly ILocalStorageService localStorage;
+		private readonly AuthenticationStateProvider authenticationStateProvider;
 
-		public AuthenticationService(IClient httpClient, ILocalStorageService localStorage)
+		public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
 		{
 			this.httpClient = httpClient;
 			this.localStorage = localStorage;
+			this.authenticationStateProvider = authenticationStateProvider;
 		}
 
 		public async Task<bool> AuthenticateAsync(LoginUserDto loginModel)
