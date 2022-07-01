682f420bb28fa8814876ead39491564d62047a96
adipati27ma <adipati.cs.samsung@gmail.com>
2022-07-01 22:14:34 +0700

    [checkpoint] create books

--- BookStoreApp.API/Models/Book/BookCreateDto.cs
+++ BookStoreApp.API/Models/Book/BookCreateDto.cs
@@ -2,27 +2,30 @@
 
 namespace BookStoreApp.API.Models.Book
 {
-    public class BookCreateDto
-    {
-        [Required]
-        [StringLength(50)]
-        public string Title { get; set; }
+	public class BookCreateDto
+	{
+		[Required]
+		public int AuthorId { get; set; }
 
-        [Required]
-        [Range(1000, int.MaxValue)]
-        public int Year { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Title { get; set; }
 
-        [Required]
-        public string Isbn { get; set; }
+		[Required]
+		[Range(1000, int.MaxValue)]
+		public int Year { get; set; }
 
-        [Required]
-        [StringLength(250, MinimumLength = 10)]
-        public string Summary { get; set; }
+		[Required]
+		public string Isbn { get; set; }
 
-        public string Image { get; set; }
+		[Required]
+		[StringLength(250, MinimumLength = 10)]
+		public string Summary { get; set; }
 
-        [Required]
-        [Range(0, int.MaxValue)]
-        public decimal Price { get; set; }
-    }
+		public string Image { get; set; }
+
+		[Required]
+		[Range(0, int.MaxValue)]
+		public decimal Price { get; set; }
+	}
 }
